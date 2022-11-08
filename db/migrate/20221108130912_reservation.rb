b75bc4dd1f9f377f0a455a40d8dd61f804a53b77
ogola5 <ogolaevance5@gmail.com>
2022-11-08 16:16:03 +0300

    completed

--- db/migrate/20221108130912_reservation.rb
+++ db/migrate/20221108130912_reservation.rb
@@ -1,12 +1,12 @@
 class Reservation < ActiveRecord::Migration[6.1]
   def change
-    create_table :reservation do
+    create_table :reservation do |t|
       t.integer :customer_id
       t.integer :restaurant_id
       t.integer :seat
       t.integer :time
       t.boolean :true
-      
+
       t.timestamps
 
     end
